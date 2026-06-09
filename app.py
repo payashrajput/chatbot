aed1aa549de12c7c1f1c8264f5f6e4e5239d2fda
payashrajput <rajputpayash@gmail.com>
2026-06-09 15:12:56 +0530

    Update Hugging Face API token in app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def load_model(temp, max_new_tokens):
         repo_id="deepseek-ai/DeepSeek-V4-Pro",
         provider="hf-inference",
         task="text-generation",
-        huggingfacehub_api_token=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,
+        huggingfacehub_api_token=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,
         temperature=temp,
         max_new_tokens=max_new_tokens
     )
